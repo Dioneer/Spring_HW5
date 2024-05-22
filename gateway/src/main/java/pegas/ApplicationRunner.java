792f485297f0a084250016be679fe6c6e51f7d51
Dioneer <gulkina_em@mail.ru>
2024-05-22 20:18:04 +0300

    add gateway

---
+++ gateway/src/main/java/pegas/ApplicationRunner.java
@@ -0,0 +1,21 @@
+package pegas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class,args);
+    }
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("app1", r->r.path("/tasks/**").uri("http://localhost:8080/"))
+                .route("app1", r->r.path("/upload/**").uri("http://localhost:8080/"))
+                .build();
+    }
+}
